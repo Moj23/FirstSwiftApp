a6b65a73dbf067f8c023b261962930055b68ea03
Manoj Fernando <manojfernando@Manojs-MacBook-Air.local>
2018-01-17 17:18:05 -0500

    Initial Commit

---
+++ First Swift app Class/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  First Swift app Class
+//
+//  Created by Manoj Fernando on 2018-01-17.
+//  Copyright © 2018 Mojo Code. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
